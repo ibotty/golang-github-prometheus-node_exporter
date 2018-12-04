4c25f9c9927b3fbd4786bdf4ff8e9ffcff52076b
Tobias Florek <tob@butter.sh>
2018-12-04 15:24:09 +0100

    add script to help in making new releases

---
+++ bump_version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+VERSION="$1"
+OLDVERSION="$(awk '/^Version: /{print $2}' golang-github-prometheus-node_exporter.spec)"
+
+echo "downloading source v$VERSION"
+curl -sSLO "https://github.com/prometheus/node_exporter/archive/v${VERSION}.tar.gz"
+
+echo "change version in spec file"
+sed -i '/^Version:/s/[0-9.]*$/'"$VERSION"'/' golang-github-prometheus-node_exporter.spec
+
+echo "remove old source tarball and add new one"
+git rm "v$OLDVERSION.tar.gz"
+git add "v$VERSION.tar.gz"
+
+echo "commit changes"
+git commit -m "bump version to v$VERSION"
+
+echo "Now what's left is tito tag and git push --follow-tags origin"
